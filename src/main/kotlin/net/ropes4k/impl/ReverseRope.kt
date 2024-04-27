d8fa0982f007530d48595bec5a7e51e18c18129e
James Richardson <james@time4tea.net>
2024-04-27 16:17:47 +0100

    move to kotlin

--- src/main/kotlin/net/ropes4k/impl/ReverseRope.kt
+++ src/main/kotlin/net/ropes4k/impl/ReverseRope.kt
@@ -3,13 +3,12 @@
  *  - Originally Copyright (C) 2007 Amin Ahmad.
  * Licenced under GPL
  */
-package net.ropes4k.impl;
+package net.ropes4k.impl
 
-import net.ropes4k.Rope;
-
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Iterator;
+import net.ropes4k.Rope
+import net.ropes4k.impl.RopeUtilities.Companion.depth
+import java.io.IOException
+import java.io.Writer
 
 /**
  * A rope representing the reversal of character sequence.
@@ -17,105 +16,80 @@ import java.util.Iterator;
  *
  * @author Amin Ahmad
  */
-public final class ReverseRope extends AbstractRope {
-
-    private final Rope rope;
-
-    /**
-     * Constructs a new rope from an underlying rope.
-     * <p>
-     * Balancing algorithm works optimally when only FlatRopes or
-     * SubstringRopes are supplied. Framework must guarantee this
-     * as no runtime check is performed.
-     */
-    public ReverseRope(Rope rope) {
-        this.rope = rope;
+class ReverseRope
+/**
+ * Constructs a new rope from an underlying rope.
+ *
+ *
+ * Balancing algorithm works optimally when only FlatRopes or
+ * SubstringRopes are supplied. Framework must guarantee this
+ * as no runtime check is performed.
+ */(private val rope: Rope) : AbstractRope() {
+    override fun get(index: Int): Char {
+        return rope[length - index - 1]
     }
 
-    @Override
-    public char charAt(int index) {
-        return rope.charAt(length() - index - 1);
+    override fun depth(): Int {
+        return depth(rope)
     }
 
-    @Override
-    public int depth() {
-        return RopeUtilities.Companion.depth(rope);
-    }
+    override fun iterator(start: Int): Iterator<Char> {
+        if (start < 0 || start > length) throw IndexOutOfBoundsException("Rope index out of range: $start")
+        return object : MutableIterator<Char> {
+            var current: Int = start
 
-    @Override
-    public Iterator<Character> iterator(int start) {
-        if (start < 0 || start > length())
-            throw new IndexOutOfBoundsException("Rope index out of range: " + start);
-        return new Iterator<>() {
-            int current = start;
-
-            @Override
-            public boolean hasNext() {
-                return current < length();
+            override fun hasNext(): Boolean {
+                return current < length
             }
 
-            @Override
-            public Character next() {
-                return charAt(current++);
+            override fun next(): Char {
+                return get(current++)
             }
 
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException("Rope iterator is read-only.");
+            override fun remove() {
+                throw UnsupportedOperationException("Rope iterator is read-only.")
             }
-        };
+        }
     }
 
-    @Override
-    public int length() {
-        return rope.length();
+    override val length: Int get() = rope.length
+
+    override fun reverse(): Rope {
+        return rope
     }
 
-    @Override
-    public Rope reverse() {
-        return rope;
-    }
+    override fun reverseIterator(start: Int): Iterator<Char> {
+        if (start < 0 || start > length) throw IndexOutOfBoundsException("Rope index out of range: $start")
+        return object : MutableIterator<Char> {
+            var current: Int = length - start
 
-    public Iterator<Character> reverseIterator(int start) {
-        if (start < 0 || start > length())
-            throw new IndexOutOfBoundsException("Rope index out of range: " + start);
-        return new Iterator<>() {
-            int current = length() - start;
-
-            @Override
-            public boolean hasNext() {
-                return current > 0;
+            override fun hasNext(): Boolean {
+                return current > 0
             }
 
-            @Override
-            public Character next() {
-                return charAt(--current);
+            override fun next(): Char {
+                return get(--current)
             }
 
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException("Rope iterator is read-only.");
+            override fun remove() {
+                throw UnsupportedOperationException("Rope iterator is read-only.")
             }
-        };
+        }
     }
 
-    @Override
-    public Rope subSequence(int start, int end) {
-        if (start == 0 && end == length())
-            return this;
-        return rope.subSequence(length() - end, length() - start).reverse();
+    override fun subSequence(start: Int, end: Int): Rope {
+        if (start == 0 && end == length) return this
+        return rope.subSequence(length - end, length - start).reverse()
     }
 
-    @Override
-    public void write(Writer out) throws IOException {
-        write(out, 0, length());
+    @Throws(IOException::class)
+    override fun write(out: Writer) {
+        write(out, 0, length)
     }
 
-    @Override
-    public void write(Writer out, int offset, int length) throws IOException {
-        if (offset < 0 || offset + length > length())
-            throw new IndexOutOfBoundsException("Rope index out of bounds:" + (offset < 0 ? offset : offset + length));
-        for (int j = offset; j < offset + length; ++j)
-            out.write(charAt(j));
+    @Throws(IOException::class)
+    override fun write(out: Writer, offset: Int, length: Int) {
+        if (offset < 0 || offset + length > length) throw IndexOutOfBoundsException("Rope index out of bounds:" + (if (offset < 0) offset else offset + length))
+        for (j in offset until offset + length) out.write(get(j).code)
     }
 }
