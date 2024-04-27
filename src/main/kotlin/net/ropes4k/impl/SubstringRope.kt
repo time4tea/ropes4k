b80324bc9ae3df7a393f5b4856cbee772c0cf0b2
James Richardson <james@time4tea.net>
2024-04-27 16:08:43 +0100

    move to kotlin

--- src/main/kotlin/net/ropes4k/impl/SubstringRope.kt
+++ src/main/kotlin/net/ropes4k/impl/SubstringRope.kt
@@ -3,46 +3,31 @@
  *  - Originally Copyright (C) 2007 Amin Ahmad.
  * Licenced under GPL
  */
-package net.ropes4k.impl;
+package net.ropes4k.impl
 
-import net.ropes4k.Rope;
-
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Iterator;
+import net.ropes4k.Rope
+import net.ropes4k.impl.RopeUtilities.Companion.depth
+import java.io.IOException
+import java.io.Writer
 
 /**
  * Represents a lazily-evaluated substring of another rope. For performance
- * reasons, the target rope must be a <code>FlatRope</code>.
- *
- * @author aahmad
+ * reasons, the target rope must be a `FlatRope`.
  */
-public class SubstringRope extends AbstractRope {
+class SubstringRope(val rope: FlatRope, val offset: Int, override val length: Int) : AbstractRope() {
 
-    private final FlatRope rope;
-    private final int offset;
-    private final int length;
-
-    public SubstringRope(FlatRope rope, int offset, int length) {
-        if (length < 0 || offset < 0 || offset + length > rope.length())
-            throw new IndexOutOfBoundsException("Invalid substring offset (" + offset + ") and length (" + length + ") for underlying rope with length " + rope.length());
-
-        this.rope = rope;
-        this.offset = offset;
-        this.length = length;
+    init {
+        if (length < 0 || offset < 0 || offset + length > rope.length) throw IndexOutOfBoundsException("Invalid substring offset (" + offset + ") and length (" + length + ") for underlying rope with length " + rope.length)
     }
 
-    @Override
-    public char charAt(int index) {
-        if (index >= length())
-            throw new IndexOutOfBoundsException("Rope index out of range: " + index);
+    override fun get(index: Int): Char {
+        if (index >= length) throw IndexOutOfBoundsException("Rope index out of range: $index")
 
-        return rope.charAt(offset + index);
+        return rope[offset + index]
     }
 
-    @Override
-    public byte depth() {
-        return RopeUtilities.Companion.depth(getRope());
+    override fun depth(): Byte {
+        return depth(getRope())
     }
 
     /**
@@ -50,93 +35,64 @@ public class SubstringRope extends AbstractRope {
      *
      * @return the rope underlying this one.
      */
-    public Rope getRope() {
-        return rope;
+    fun getRope(): Rope {
+        return rope
     }
 
-    @Override
-    public Iterator<Character> iterator(int start) {
-        if (start < 0 || start > length())
-            throw new IndexOutOfBoundsException("Rope index out of range: " + start);
-        return new Iterator<>() {
+    override fun iterator(start: Int): Iterator<Char> {
+        if (start < 0 || start > length) throw IndexOutOfBoundsException("Rope index out of range: $start")
+        return object : Iterator<Char> {
+            val u: Iterator<Char> = getRope().iterator(offset + start)
+            var position: Int = start
 
-            final Iterator<Character> u = getRope().iterator(offset + start);
-            int position = start;
-
-            @Override
-            public boolean hasNext() {
-                return position < length();
+            override fun hasNext(): Boolean {
+                return position < length
             }
 
-            @Override
-            public Character next() {
-                ++position;
-                return u.next();
+            override fun next(): Char {
+                ++position
+                return u.next()
+            }
+        }
+    }
+
+    override fun reverse(): Rope {
+        return ReverseRope(this)
+    }
+
+    override fun reverseIterator(start: Int): Iterator<Char> {
+        if (start < 0 || start > length) throw IndexOutOfBoundsException("Rope index out of range: $start")
+        return object : Iterator<Char> {
+            val u: Iterator<Char> = getRope().reverseIterator(getRope().length - offset - length + start)
+            var position: Int = length - start
+
+            override fun hasNext(): Boolean {
+                return position > 0
             }
 
-            @Override
-            public void remove() {
-                u.remove();
+            override fun next(): Char {
+                --position
+                return u.next()
             }
-
-        };
+        }
     }
 
-    @Override
-    public int length() {
-        return length;
+    override fun subSequence(startIndex: Int, endIndex: Int): Rope {
+        if (startIndex == 0 && endIndex == length) return this
+        return SubstringRope(rope, offset + startIndex, endIndex - startIndex)
     }
 
-    @Override
-    public Rope reverse() {
-        return new ReverseRope(this);
+    override fun toString(): String {
+        return rope.toString(offset, length)
     }
 
-    @Override
-    public Iterator<Character> reverseIterator(int start) {
-        if (start < 0 || start > length())
-            throw new IndexOutOfBoundsException("Rope index out of range: " + start);
-        return new Iterator<>() {
-            final Iterator<Character> u = getRope().reverseIterator(getRope().length() - offset - length() + start);
-            int position = length() - start;
-
-            @Override
-            public boolean hasNext() {
-                return position > 0;
-            }
-
-            @Override
-            public Character next() {
-                --position;
-                return u.next();
-            }
-
-            @Override
-            public void remove() {
-                u.remove();
-            }
-        };
+    @Throws(IOException::class)
+    override fun write(out: Writer) {
+        rope.write(out, offset, length)
     }
 
-    @Override
-    public Rope subSequence(int start, int end) {
-        if (start == 0 && end == length())
-            return this;
-        return new SubstringRope(rope, offset + start, end - start);
-    }
-
-	@Override
-    public String toString() {
-        return rope.toString(offset, length);
-    }
-
-    @Override
-    public void write(Writer out) throws IOException {
-        rope.write(out, offset, length);
-    }
-
-    @Override
-    public void write(Writer out, int offset, int length) throws IOException {
-        rope.write(out, this.offset + offset, length);
+    @Throws(IOException::class)
+    override fun write(out: Writer, offset: Int, length: Int) {
+        rope.write(out, this.offset + offset, length)
     }
 }
