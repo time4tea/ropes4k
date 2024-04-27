358f5e9926e70649061bb3ed53585f4b61cd291a
James Richardson <james@time4tea.net>
2024-04-27 19:58:25 +0100

    add creation benchmark
    highlights copy in FlatCharArrayRope

---
+++ src/test/kotlin/net/ropes4k/test/bench/CreationBenchmark.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024
+ *  - Originally Copyright (C) 2007 Amin Ahmad.
+ * Licenced under GPL
+ */
+
+package net.ropes4k.test.bench
+
+import net.ropes4k.Rope
+import net.ropes4k.test.PerformanceTest.Companion.aChristmasCarol
+import net.ropes4k.test.PerformanceTest.Companion.aChristmasCarolRaw
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Fork
+import org.openjdk.jmh.annotations.Measurement
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.Warmup
+import java.lang.String
+import java.util.concurrent.TimeUnit
+
+@State(Scope.Benchmark)
+@Fork(1)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+open class CreationBenchmark {
+
+
+    @Benchmark
+    fun string(): Any {
+        return String(aChristmasCarolRaw)
+    }
+
+    @Benchmark
+    fun stringbuilder(): Any {
+        return StringBuilder(aChristmasCarol)
+    }
+
+    @Benchmark
+    fun rope_charsequence(): Any {
+        return Rope.of(aChristmasCarol)
+    }
+
+    @Benchmark
+    fun rope_chararray(): Any {
+        return Rope.of(aChristmasCarolRaw)
+    }
+}
