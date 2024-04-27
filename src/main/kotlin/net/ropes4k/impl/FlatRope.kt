fdad087f363f6453f2e2419bb415c723da4f063e
James Richardson <james@time4tea.net>
2024-04-27 14:11:08 +0100

    move FlatRope to kotlin

--- src/main/kotlin/net/ropes4k/impl/FlatRope.kt
+++ src/main/kotlin/net/ropes4k/impl/FlatRope.kt
@@ -3,24 +3,24 @@
  *  - Originally Copyright (C) 2007 Amin Ahmad.
  * Licenced under GPL
  */
-package net.ropes4k.impl;
+package net.ropes4k.impl
 
-import net.ropes4k.Rope;
+import net.ropes4k.Rope
 
 /**
  * A rope that is directly backed by a data source.
  *
  * @author Amin Ahmad
  */
-public interface FlatRope extends Rope {
+interface FlatRope : Rope {
     /**
-     * Returns a <code>String</code> representation of a range
+     * Returns a `String` representation of a range
      * in this rope.
      *
      * @param offset the offset.
      * @param length the length.
-     * @return a <code>String</code> representation of a range
+     * @return a `String` representation of a range
      * in this rope.
      */
-    String toString(int offset, int length);
+    fun toString(offset: Int, length: Int): String
 }
